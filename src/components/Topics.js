4584b7513c39c956bc3942c18805cc4da74572e7
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-01-01 17:07:39 -0800

    add router

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
 
-export default function Topics() {
-  return <div>Topics</div>;
+function Topic({ match }) {
+  return <h3>{match.params.topicId}</h3>;
+}
+
+export default function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Render with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/rendering2`}>Render with React2</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/rendering3`}>Render with React3</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      {/* <Route
+        path={`${match.path}/:topicId`}
+        render={() => {
+          return <Topic name="name" />;
+        }}
+      /> */}
+      <Route
+        exact
+        path={match.path}
+        render={() => {
+          return <h4>Please select a topic</h4>;
+        }}
+      />
+    </div>
+  );
 }
